8d9b3da38947f11cd68cafefb188adddc59808f4
nyakori <935419854@qq.com>
2023-03-14 12:58:30 +0800

    fix(store): 加一些注释

--- src/store/sidebar.ts
+++ src/store/sidebar.ts
@@ -1,3 +1,17 @@
+/*********************************************************
+ * File: sidebar.ts
+ * Created Date: 2023-03-13
+ * Author: walnut(覃鹏展)
+ * 
+ * Description:
+ * 	商品：
+ * 		1. 侧边栏折叠状态
+ * 	功能：
+ * 		1. 控制侧边栏状态
+ * 
+ * Copyright (C) 2023 襄阳市中心医院
+ *********************************************************/
+
 import { defineStore } from 'pinia';
 
 export const useSidebarStore = defineStore('sidebar', {
